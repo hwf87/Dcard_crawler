2495126358908e1353f4d2f3dcbe6ca953f47dbf
jackyfu <>
2021-09-02 23:41:42 +0800

    edit _run_script

--- _run_test_script.sh
+++ _run_test_script.sh
@@ -1,7 +1,7 @@
 echo 'start dcard daily etl job'
 source /Users/jackyfu/miniforge3/bin/activate
 cd /Users/jackyfu/Desktop/hwf87_git/Dcard_crawler/
-python3 utils.py
+python3 postCrawler.py
 #END=$(date + "%Y%m%d, %H:%M")
 echo 'end dcard daily etl job'
 
